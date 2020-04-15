addec3929416da4edc8b8657c69239c1f3491ec0
Matthew Carroll <matthew.j.carroll@outlook.com>
2020-04-14 19:15:15 -0500

    adding files here so they do not get deleted again

--- trust-assessment.js
+++ trust-assessment.js
@@ -1,7 +1,3 @@
-var countmismatch = 0;
-var countundecided = 0;
-var countmatch = 0;
-
 function zip() {
     var args = [].slice.call(arguments);
     var shortest = args.length==0 ? [] : args.reduce(function(a,b){
@@ -13,49 +9,64 @@ function zip() {
     });
 }
 
-var tempArray = zip(templist, d)
+function getCount(){
+    var countmismatch = 0;
+    var countundecided = 0;
+    var countmatch = 0;
+    var tempArray = zip(templist, d)
 
-tempArray.array.forEach(element => {
-    var a = element[0];
-    var b = element[1];
-    if(a == 'Yes' && (b == 'R' || b == null)){
-        countmatch++;
-    }
-    else if(a == 'No' && b == 'R'){
-        countmismatch++;
-    }
-    else if(a == 'No' && b == null){
-        countmatch++;
-    }
-    else if(a == 'Uncertain' && b == 'R'){
-        countundecided++;
-    }
-    else if(a == 'Uncertain' && b == null){
-        countmatch++;
-    }
-});
+    tempArray.array.forEach(element => {
+        var a = element[0];
+        var b = element[1];
+        if(a == 'Yes' && (b == 'R' || b == null)){
+            countmatch++;
+        }
+        else if(a == 'No' && b == 'R'){
+            countmismatch++;
+        }
+        else if(a == 'No' && b == null){
+            countmatch++;
+        }
+        else if(a == 'Uncertain' && b == 'R'){
+            countundecided++;
+        }
+        else if(a == 'Uncertain' && b == null){
+            countmatch++;
+        }
+    });
 
-var n = 14;
-var alpha_c = Math.floor(countmatch + ((countundecided*countmatch)/(countmatch+countmismatch)));
-var beta_c = n - alpha_c;
-var ei = float(alpha_c + 1)/float(alpha_c + beta_c + 2);
-// console.log('Beta model is ' + ei);
+    var n = 14;
+    var alpha_c = Math.floor(countmatch + ((countundecided*countmatch)/(countmatch+countmismatch)));
+    var beta_c = n - alpha_c;
+    var ei = float(alpha_c + 1)/float(alpha_c + beta_c + 2);
+    // console.log('Beta model is ' + ei);
 
-var a = 0.7
-var eb = float(countmatch+1.0)/float(countmatch+countmismatch+countundecided+3.0);
-var eu = float(countundecided+1.0)/float(countmatch+countmismatch+countundecided+3.0);
-var ew = (eb + a*eu);
-var rEw = Math.log(ew)/Math.log((1-ew));
-var wi = 0;
-// console.log('rEw: ' + rEw);
-if(rEw > 0){
-    wi = 1 - Math.exp(-(Math.abs(rEw)));
+    var a = 0.7
+    var eb = float(countmatch+1.0)/float(countmatch+countmismatch+countundecided+3.0);
+    var eu = float(countundecided+1.0)/float(countmatch+countmismatch+countundecided+3.0);
+    var ew = (eb + a*eu);
+    var rEw = Math.log(ew)/Math.log((1-ew));
+    var wi = 0;
+    // console.log('rEw: ' + rEw);
+    if(rEw > 0){
+        wi = 1 - Math.exp(-(Math.abs(rEw)));
+    }
+    else if(rEw < 0){
+        wi = -(1 - Math.exp(-(Math.abs(rEw))));
+    }
+    else{
+        wi = 0;
+    }
+    // console.log('dirichlet model is ' + wi);
 }
-else if(rEw < 0){
-    wi = -(1 - Math.exp(-(Math.abs(rEw))));
-}
-else{
-    wi = 0;
-}
-// console.log('dirichlet model is ' + wi);
 
+/**
+ * Track the request of a dataset from one user to another
+ * @param {org.example.mynetwork.Request} request - the request to be processed
+ * @transaction
+ */
+async function tradeCommodity(request) {
+    request.dataset.user = request.newUser;
+    let assetRegistry = await getAssetRegistry('org.example.mynetwork.Dataset');
+    await assetRegistry.update(request.dataset);
+}
